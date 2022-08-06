2adaa7d5c9784608b56b2977319aa5cda8449fa8
Lounes Badji <lounes.b.ext@padoa-group.com>
2022-08-06 14:25:12 +0200

    feat: upgrade dependencies + swagger added

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,29 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerOptions = new DocumentBuilder()
+    .setTitle('DzDialect Identity API')
+    .setDescription('DzDialect Identity API Documentation')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerOptions);
+
+  SwaggerModule.setup('openapi', app, document, {
+    swaggerOptions: {
+      displayRequestDuration: true,
+      docExpansion: 'none',
+      operationsSorter: 'alpha',
+      tagsSorter: 'alpha',
+    },
+  });
+
   await app.listen(3000);
 }
 bootstrap();
